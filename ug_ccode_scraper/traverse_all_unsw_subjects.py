3d7de3b0ad3e056aa613a441da02c30589dd25ea
Seng Yong Hoo <hoosyong@gmail.com>
2021-06-23 10:07:01 +1000

    renamed code scraper directory

--- ug_ccode_scraper/traverse_all_unsw_subjects.py
+++ ug_ccode_scraper/traverse_all_unsw_subjects.py
@@ -1,4 +1,4 @@
-from .traverse_all_courses import traverse_all_courses
+from traverse_all_courses import traverse_all_courses
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -12,13 +12,13 @@ def traverse_all_subjects(driver):
   subject_page_xpath = "//li[contains(@class, 'react-tabs__tab') and text()[contains(.,'By Subject Area')]]"
   subject_link = driver.find_element_by_xpath(subject_page_xpath)
   subject_link.click()
-  # find all subject area links
+  # find all subject links
   subject_list_xpath = "//a[contains(@href,'/browse/By Subject Area/')]/div[contains(@class,'css-14ezrsm-STileHeader e16r0ulu2')]"
   subject_list = driver.find_elements_by_xpath(subject_list_xpath)
   curr_url = driver.current_url
   print(curr_url)
   
-  WebDriverWait(driver, 5).until(EC.visibility_of_all_elements_located((By.XPATH,subject_list_xpath)))
+  WebDriverWait(driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH,subject_list_xpath)))
 
   subject_nlist = []
   for subject in subject_list:
@@ -26,7 +26,7 @@ def traverse_all_subjects(driver):
     subject_nlist.append(subject_codename)
 
   i = 1
-  start_here = False
+  start_here = True
   for subjectn in subject_nlist:
     subjectn = subjectn.replace('&amp;','&')
     sn_xpath = "//h4[contains(text(), '" + subjectn + "')]"
--- ug_ccode_scraper/unsw_handscraper.py
+++ ug_ccode_scraper/unsw_handscraper.py
@@ -3,7 +3,7 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
 from fake_useragent import UserAgent
 import os
-from .traverse_all_unsw_subjects import traverse_all_subjects
+from traverse_all_unsw_subjects import traverse_all_subjects
 
 def main():
   opts = Options()
@@ -15,7 +15,8 @@ def main():
   #opts.add_argument('--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36"')
   opts.add_argument("--window-size=1920,1080")
   #assert opts.headless
-  chrome_driver = os.getcwd() + '/chromedriver'
+  chrome_driver = os.getcwd() + '/../chromedriver'
+  print(chrome_driver)
   
 
   driver = Chrome(options=opts, executable_path=chrome_driver)
