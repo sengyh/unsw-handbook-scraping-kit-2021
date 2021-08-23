75e2a03fd4eade62d12657d103a936ab38e5093b
Seng Yong Hoo <hoosyong@gmail.com>
2021-08-23 23:45:10 +1000

    wrote some more print helpers, will start working on parsing specialisations tmr, i swear this commit is not made purely to keep up my commit streak! :P

--- refiners/specialisations/print_helpers.ts
+++ refiners/specialisations/print_helpers.ts
@@ -11,15 +11,52 @@ const explore_specs = (): void => {
       if (key === 'default') continue;
       const struct_section_title: string = key;
       const struct_section_body: SpecStructBody = val; 
-      if (struct_section_body.description.match(/[a-z]{4}[0-9]{4}/gmi) && struct_section_body.courses.length === 0) {
-        console.log(spec_code + ': ' + struct_section_title);
-        console.log('uoc: ' + struct_section_body.uoc)
-        console.log(struct_section_body.description.replaceAll(/\n{2,}/gm, '\n'));
-        console.log(struct_section_body.courses)
-        console.log('\n')
-      }
-      //console.log(struct_section_body.description.replaceAll(/\n/gm, '  ###  '));
+      //no_courses_in_struct_sect(spec_code, struct_section_title, struct_section_body);
+      //no_uoc_in_struct_sect(spec_code, struct_section_title, struct_section_body);
+      //dnc_have_courses(spec_code, struct_section_title, struct_section_body);
+      //console.log(struct_section_body.description.replaceAll(/\n/gm, '\t\t'));
+      console.log(struct_section_body.courses)
     }
+  }
+  return;
+}
+
+const no_courses_in_struct_sect = (spec_code: string, struct_section_title: string, struct_section_body: SpecStructBody): void => {
+  if (struct_section_body.description.match(/[a-z]{4}[0-9]{4}/gmi) && struct_section_body.courses.length === 0) {
+    console.log(spec_code + ': ' + struct_section_title);
+    console.log('uoc: ' + struct_section_body.uoc)
+    console.log(struct_section_body.description.replaceAll(/\n{2,}/gm, '\n'));
+    console.log(struct_section_body.courses)
+    console.log('\n')
+  }
+  return;
+}
+
+const no_uoc_in_struct_sect = (spec_code: string, struct_section_title: string, struct_section_body: SpecStructBody): void => {
+  if (struct_section_body.uoc === "") {
+    console.log(spec_code + ': ' + struct_section_title);
+    console.log(struct_section_body.description.replaceAll(/\n{2,}/gm, '\n'));
+    console.log(struct_section_body.courses)
+    console.log('\n')
+  }
+  return;
+}
+
+const dnc_have_courses = (spec_code: string, struct_section_title: string, struct_section_body: SpecStructBody): void => {
+  if (struct_section_body.courses.length > 0 && struct_section_body.description.match(/[a-z]{4}\d{4}/gmi)) {
+    console.log(spec_code + ': ' + struct_section_title);
+    console.log(struct_section_body.description.replaceAll(/\n{2,}/gm, '\n'));
+    console.log(struct_section_body.courses)
+    console.log('\n')
+  }
+  return;
+}
+
+// only case where this happens (ACCTA1: CAANZ/CPA Accreditation Requirements)
+const title_only_in_struct_sect = (spec_code: string, struct_section_title: string, struct_section_body: SpecStructBody): void => {
+  if (struct_section_body.uoc === "" && struct_section_body.description === "" && struct_section_body.courses.length === 0) {
+    console.log(spec_code + ': ' + struct_section_title);
+    console.log('\n')
   }
   return;
 }
