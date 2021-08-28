a5f78f06e2670dd5bb1ebc1fbb1236d65a3b15ba
Seng Yong Hoo <hoosyong@gmail.com>
2021-08-29 00:06:07 +1000

    processed uoc (both min max) if there are courses in the struct element but uoc attribute is empty

--- refiners/specialisations/process_spec_structure.ts
+++ refiners/specialisations/process_spec_structure.ts
@@ -3,6 +3,7 @@ import type { SpecStructure, SpecStructBody, ProcessedStructBody, SubValKeyObj, 
 import {clean_course_group_str, construct_unlocked_by_arr} from '../courses/prereq_section_helpers';
 import { extract_all_found_courses } from './spec_element_helper';
 import * as _ from 'lodash';
+import { min } from 'lodash';
 
 export const process_spec_structure = (spec_structure: SpecStructure): void => {
   let processed_structure_arr: ProcessedStructBody[] = [];
@@ -18,11 +19,63 @@ export const process_spec_structure = (spec_structure: SpecStructure): void => {
 
 const construct_spec_element = (title: string, body: SpecStructBody): void => { 
   // going to trust (lol) that the handbook ppl know their shit and that they did not leave any courses behind if the section already has courses. also, screw usyd.
+  let new_body: SpecStructBody = _.cloneDeep(body);
   if (body.courses.length === 0 && body.description !== "") {
     const all_found_courses: string[] = extract_all_found_courses(body);
-    console.log(body.description)
-    console.log(all_found_courses)
-    console.log('')
+    new_body.courses = all_found_courses;
+  }
+  // get min uoc if there are courses but no uoc entry
+  let new_uoc: string = "";
+  let min_uoc: string = "";
+  let max_uoc: string = "";
+  if (new_body.courses.length > 0 && new_body.uoc === "") {
+    min_uoc = extract_min_uoc(new_body.description);
+    max_uoc = extract_max_uoc(new_body.description);
+    new_uoc = get_new_uoc(min_uoc, max_uoc);
+    //console.log(_.trim(new_body.description, ' \n').split('\n')[0]);
+    //console.log(new_uoc + '\n');
   }
   return;
+}
+
+const extract_min_uoc = (desc_str: string): string => {
+  const first_line: string = _.trim(desc_str, ' \n').split('\n')[0];
+  const min_uoc_pattern: RegExp = /must (take|complete)( a minimum of| at least)? [0-9]+ uoc/gmi;
+  let min_uoc: string = "";
+  const min_uoc_match = first_line.match(min_uoc_pattern);
+  if (!first_line.match(/^if/gmi) && min_uoc_match) {
+    min_uoc = min_uoc_match[0].replace(/[^\d]/gm, '');
+  }
+  return min_uoc;
+}
+
+const extract_max_uoc = (desc_str: string): string => {
+  const first_line: string = _.trim(desc_str, ' \n').split('\n')[0];
+  const max_uoc_pattern: RegExp = /( up to| a maximum of) [0-9]+ uoc/gmi
+  let max_uoc: string = "";
+  const max_uoc_match = first_line.match(max_uoc_pattern);
+  if (max_uoc_match) {
+    max_uoc = max_uoc_match[0].replace(/[^\d]/gm, '');
+  }
+  return max_uoc;
+}
+
+const get_new_uoc = (min_uoc: string, max_uoc: string): string => {
+  let new_uoc: string = "";
+  if (max_uoc !== "") {
+    if (min_uoc !== "" ) {
+      if (min_uoc === max_uoc) {
+        new_uoc = min_uoc;
+      } else {
+        new_uoc = min_uoc + '-' + max_uoc;
+      }     
+    } else {
+      new_uoc = '0-' + max_uoc
+    }
+  } else {
+    if (min_uoc !== "") {
+      new_uoc = min_uoc;
+    }
+  }
+  return new_uoc;
 }
